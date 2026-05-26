99ed8b98ec3093e667b3309ac2f90de14b167760
Faid Rihan <Faid.Rihan@Tum.de>
2026-05-26 22:29:50 +0200

    add trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://h2faid.me",
+  trailingSlash: "always",
+
   vite: {
     plugins: [tailwindcss()],
   },
